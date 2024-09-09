870780beeecd789475934300d2a347277cf588b1
kzyanj <kensleyzhouyanjun023@gmail.com>
2024-09-09 10:38:51 -0400

    Lab1 FizzBuzz

---
+++ src/FizzBuzzMain.java
@@ -0,0 +1,9 @@
+public class FizzBuzzMain {
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(10);    // take a parameter in the bracket
+        FizzBuzz.doFizzBuzz(5);
+        FizzBuzz.doFizzBuzz(6);
+        FizzBuzz.doFizzBuzz(1);
+    }
+}
+// this why we wanna do refractor, we can do test outside(decouple/refractor)
